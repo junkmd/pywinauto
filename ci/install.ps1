fe89a626e11f0c8654e0f2fcc972aee59627f05c
junkmd <jun.k.job.finding12@gmail.com>
2024-10-14 14:18:25 +0900

    test enthought/comtypes#641

--- ci/install.ps1
+++ ci/install.ps1
@@ -86,7 +86,7 @@ function UpdateConda ($python_home) {
 
 function InstallComtypes ($python_home) {
     $pip_path = $python_home + "\Scripts\pip.exe"
-    $args = "install comtypes"
+    $args = "install https://github.com/newwingbird/comtypes/archive/refs/heads/prevent-itf.zip"
     Start-Process -FilePath "$pip_path" -ArgumentList $args -Wait -Passthru
 }
 
